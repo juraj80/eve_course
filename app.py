dd3ac30169570baba11709b6ffcf8ba759348345
juraj Klucka <juraj80@github.com>
2018-11-19 20:13:39 +0100

    added custom validation rules and types

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-from eve import Eve
+import re
 
-app = Eve()
+from eve import Eve
+from eve.io.mongo import Validator
+
+class MyValidator(Validator):
+    def _validate_isodd(self,isodd, field, value):
+        if isodd and not bool(value & 1):
+            self._error(field, 'Value must be an odd number')
+
+    def _validate_type_email(self,field, value):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
+            self._error(field, 'Value is not a valid email')
+
+
+app = Eve(validator = MyValidator)
--- settings.py
+++ settings.py
@@ -18,6 +18,9 @@ ITEM_METHODS = ['GET','PATCH','PUT','DELETE']
 # if False, we need to add an if match header every single time we perform an edit operation on a document
 IF_MATCH = False
 
+
+#EMBEDDING = False # is True by default on global level
+
 # PATCH - to edit document
 # PUT - to replace document
 # DELETE - to delete document
@@ -41,15 +44,16 @@ people_schema = {
 
     'middle_name': {
       'dependencies' : ['firstname','lastname', 'location.address'] # the field itself is not required, but if you provide a middle name
-                                                # you also must provide the first name and last name, otherwise the field
-                                                # would be rejected
+                                                                    # you also must provide the first name and last name, otherwise the field
+                                                                    # would be rejected
     },
 
     'born': {'type' : 'datetime'},
 #    'age' : {'readonly' : True},  # clients can't write or overwrite it
     'age': {
         'type': 'integer',
-        'coerce': int   # converts passed value to integer
+        'isodd' : True,
+        # 'coerce': int   # converts passed value to integer
     },
     'role' : {
         'type': 'list',
@@ -58,15 +62,15 @@ people_schema = {
     },
     'location': {
         'type' : 'dict',
-        'required': True,
+ #       'required': True,
         'schema' : {
             'address': {'type':'string'},
             'city':{'type':'string','required': True}
         },
     },
     'email': {
-        'type': 'string',
-        'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        'type': 'email'
+#        'regex': r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     },
     'prop1' : {
         'anyof_type': ['integer', 'float'],  # example of *of-rules typesaver
@@ -75,14 +79,38 @@ people_schema = {
     }
 }
 
-# every key in this dictionary is an endpoint and value is another dictionary where you configure your endpoint behavior
+works_schema = {
+    'title': {
+        'type' : 'string',
+        'required' : True,
+    },
+    'description' : {
+        'type' : 'string',
+    },
+    'owner' : {
+        'type' : 'objectid',
+        'required' : True,
+        # referential integrity constraint: value must exist in the
+        # 'people' collection. Since we aren't declaring a 'field' key,
+        # will default to 'people._id'.
+        'data_relation': {
+            'resource' : 'people',
+            #make the owner embeddable with  ?embedded={"owner":1}
+            'embeddable' : True # it will be ignored, because of embedding at resource level is turned off
+        },
+    },
+}
+
+# every key in this dictionary is an endpoint and value is another dictionary where you configure your endpoint structure
 DOMAIN = {
     'people': {
         'schema' : people_schema    #the schema for the people endpoint is previously defined schema
     },
-    'works':{                       #we want this endpoint to be read only, so we overwrite the global setting
-        'resource_methods': ['GET'],
-        'item_methods': ['GET']
+    'works':{
+        'embedding': False,  # at resource level embedding is turned off, so works can't be embedded
+        'schema' : works_schema
+        # 'resource_methods': ['GET'], #we want this endpoint to be read only, so we overwrite the global setting
+        # 'item_methods': ['GET']
     }
 
 }
